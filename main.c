6ca2e04b10da052ab594e260f7f64520d566e699
fabipv <fabianapinavera13@gmail.com>
2023-05-03 20:52:38 -0400

    Add is_equal_string function and create tareas

--- main.c
+++ main.c
@@ -8,8 +8,25 @@
 #include "List/list.h"
 #include "TreeMap/treemap.h"
 
+/*
+  función para comparar claves de tipo string
+  retorna 1 si son iguales
+*/
+int is_equal_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2)==0) return 1;
+    return 0;
+}
+
 int main(int argc, const char * argv[])
 {
+    int opcion;
+    Tarea* tarea;
+
+    TreeMap* tareas = createTreeMap(is_equal_string);
+
+
+
+
 
     return EXIT_SUCCESS;
 }

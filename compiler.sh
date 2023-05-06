35fd3e0d6404f818e3f8e21a6b28e8278219dbdd
Matias Guerrero Castillo <mati.guerrerocastillo@gmail.com>
2023-05-06 19:34:39 -0400

    Add compiler.sh script for compiling program

---
+++ compiler.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script para compilar el programa desde git bash
+
+# Ingresar el nombre del ejecutable
+read -p "Ingrese el nombre del ejecutable: " executable
+
+# Archivos a compilar
+FILES="main.c  Struct/struct.h List/list.c TreeMap/treemap.c Map/Map.c OptionsMenu/optionsMenu.c"
+
+# Compilacion con gcc
+gcc -g $FILES -o $executable
+
+# Verificar si hubo errores
+if [ $? -eq 0 ]; then
+    echo "Compilacion exitosa"
+else
+    echo "Hubo errores en la compilacion"
+fi
+
+# Ejectuar el programa en una ventana nueva
+cmd.exe /c $executable.exe

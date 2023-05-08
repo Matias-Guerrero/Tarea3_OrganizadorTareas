cf73c00a7d4a02b14a243732318766f05f9b167d
Matias Guerrero Castillo <mati.guerrerocastillo@gmail.com>
2023-05-07 22:44:56 -0400

    feat: Add agregarTarea() function to implement in menu

--- OptionsMenu/optionsMenu.c
+++ OptionsMenu/optionsMenu.c
@@ -18,11 +18,11 @@ void mostrarMenu()
     puts("     Menu: Organizador de tareas");
     puts("=======================================================");
     puts("1. Agregar tarea");
-    puts("2. Establecer precedencia entre tareas");
-    puts("3. Mostrar tareas por hacer");
-    puts("4. Marcar tarea como completada");
-    puts("5. Deshacer ultima accion");
-    puts("6. Cargar datos de tareas desde un archivo de texto");
+    puts("2. Establecer precedencia entre tareas"); // Sin implementar
+    puts("3. Mostrar tareas por hacer"); // Sin implementar
+    puts("4. Marcar tarea como completada"); // Sin implementar
+    puts("5. Deshacer ultima accion"); // Sin implementar
+    puts("6. Cargar datos de tareas desde un archivo de texto"); // Sin implementar
     puts("7. Salir");
     puts("=======================================================");
 }
@@ -34,4 +34,38 @@ void ingresarValor(char *valor, char *mensaje)
     puts("========================================");
     scanf("%30[^\n]", valor);
     while(getchar() != '\n');
+}
+
+void agregarTarea(TreeMap *arbolTareas, Map *mapaTareas, char *nombreTarea, int prioridadTarea)
+{
+    if(searchMap(mapaTareas, nombreTarea) == NULL)
+    {
+        // Crear tarea
+        Tarea *tarea = (Tarea*) malloc(sizeof(Tarea));
+
+        // Asignar valores a la tarea
+        strcpy(tarea->nombreTarea, nombreTarea);
+        tarea->prioridad = prioridadTarea;
+        strcpy(tarea->tareaPrecedente, "");
+
+        // Agregar tarea al arbol y al mapa
+        insertTreeMap(arbolTareas, (void *)tarea->prioridad, (void *)tarea);
+        insertMap(mapaTareas, tarea->nombreTarea, tarea);
+
+        system("cls");
+
+        // Mostrar mensaje de exito
+        puts("\n========================================");
+        puts("        Tarea agregada exitosamente");
+        puts("========================================\n");
+    }
+    else
+    {
+        // Mostrar mensaje de error
+        puts("\n========================================");
+        puts("          La tarea ya existe");
+        puts("========================================\n");
+    }
+
+    system("pause");
 }
